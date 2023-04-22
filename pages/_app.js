e501406a73c468c89cc2b1b4f10d244f9330c918
AldairMorales <aldairmorales@protonmail.com>
2023-04-21 22:00:29 -0500

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { ChakraProvider, Container } from '@chakra-ui/react'
+import theme from '@/config/theme'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <ChakraProvider theme={theme}>
+      <Container textAlign="center" fontSize="2xl" p="1em">
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  )
 }
